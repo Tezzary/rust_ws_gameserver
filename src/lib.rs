8eeeeb3a53c5a46e4741fe64536d3520a451c219
Zach <zacharyeverett.00@gmail.com>
2025-09-01 01:41:37 +1000

    very basic server game loop with connections and sending + receiving of text + untested ping pong setup

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,79 @@
 use tokio;
 use tokio_tungstenite;
+pub use tungstenite::protocol::Message;
+use tungstenite::protocol::frame::Utf8Bytes;
+use tungstenite::Bytes;
 use std::thread;
 use tokio::net::TcpListener;
+
 use futures_util::{StreamExt, SinkExt};
 use std::sync::mpsc;
 
-pub enum MessageType {
-    
-}
-pub struct Message<T> {
-    message_type: MessageType,
-    data: T
-}
 pub struct Connection {
-    
+    pub send_to_client: mpsc::Sender<Message>,
+    pub receive_from_client: mpsc::Receiver<Message>
 }
-pub fn run(port: i32) {
+
+impl Connection {
+    pub fn send_text(&self, string: &str) {
+        self.send_to_client.send(Message::text(string)).unwrap();
+    }
+}
+pub fn run(port: i32) -> mpsc::Receiver<Connection> {
+
+    let (send_new_connection, receive_new_connections) = mpsc::channel::<Connection>();
+
     thread::spawn(move || {
         let rt = tokio::runtime::Runtime::new().expect("Failed to start tokio runtime");
         rt.block_on(async {
             let listener = TcpListener::bind(format!("127.0.0.1:{}", port)).await.expect("Failed to bind to port");
-            let (stream, _socket_addr) = listener.accept().await.expect("Failed to accept tcp stream");
-            let stream = tokio_tungstenite::accept_async(stream).await.expect("Failed to init websocket connection");
+            loop {
+                let (stream, _socket_addr) = listener.accept().await.expect("Failed to accept tcp stream");
+                let stream = tokio_tungstenite::accept_async(stream).await.expect("Failed to init websocket connection");
 
-            let (read_stream, write_stream) = stream.split();
+                let (mut write_stream, mut read_stream) = stream.split();
 
-            let (ws_thread_read, main_thread_write) = mpsc::channel::<Message<i32>>();
-            let (main_thread_read, ws_thread_write) = mpsc::channel::<Message<i32>>();
+                let (send_to_main_thread, receive_from_client) = mpsc::channel::<Message>();
+                let (send_to_client, send_to_client_transmitter) = mpsc::channel::<Message>();
+                
+                let send_to_client_reference = send_to_client.clone();
+                //listener
+                tokio::spawn(async move {
+                    loop {
+                        let message = read_stream.next().await.expect("Failed to get message").expect("Failed to unpack message");
+                        match message {
+                            Message::Text(text) => {
+                                send_to_main_thread.send(Message::Text(text));
+                                //let string = text.as_str().to_owned();
+                                
+                               // println!("{}", string);
+                            },
+                            Message::Ping(_bytes) => {
+                                send_to_client_reference.send(Message::Pong(Bytes::from("Pong from server!!!!"))).expect("Failed to send to send thread");
+                            },
+                            _ => {
+                                println!("Unrecognised Data format");
+                            }
+                        }
+                    }
+                });
 
-            tokio::spawn(async {
-                loop {
-                    read_stream.poll_read();
-                }
-            });
+                //sender
+                tokio::spawn(async move {
+                    loop {
+                        let message: Message = send_to_client_transmitter.recv().unwrap();
+                        write_stream.send(message).await.expect("Failed to send message");
+                    }
+                });
+
+
+                let connection: Connection = Connection {
+                    send_to_client,
+                    receive_from_client
+                };
+                send_new_connection.send(connection).expect("Failed to send connection to main thread");
+            }
         });
     });
+    return receive_new_connections;
 }
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,6 +1,50 @@
 use rust_ws_gameserver;
+use rust_ws_gameserver::{Connection, Message};
+use std::thread;
 
+const TICK_RATE: f64 = 5.0;
 #[test]
 fn test1 () {
-    rust_ws_gameserver::run();
+    let listener = rust_ws_gameserver::run(3000);
+    let mut connections: Vec<Connection> = Vec::new();
+
+    loop {
+        //load new connections
+        loop {
+            match listener.try_recv() {
+                Ok(connection) => {
+                    connections.push(connection);
+                }
+                _ => {
+                    break;
+                } 
+            }
+        }
+
+        //listen to client messages
+        for i in 0..connections.len() {
+            let connection = &connections[i];
+            loop {
+                let message = match connection.receive_from_client.try_recv() {
+                    Ok(message) => {
+                        message
+                    }
+                    _ => {
+                        break;
+                    }
+                };
+                match message {
+                    Message::Text(text) => {
+                        let string = text.as_str().to_owned();
+                        connection.send_text("Hello from server!");
+                        println!("{}", string);
+                    }
+                    _ => {
+                        println!("Received not valid message")
+                    }
+                }
+            }
+        }
+        thread::sleep(std::time::Duration::from_micros((1_000_000.0 / TICK_RATE) as u64));
+    }
 }
